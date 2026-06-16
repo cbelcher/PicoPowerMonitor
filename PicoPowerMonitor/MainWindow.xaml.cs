1216ce77b05a50dd50b56986bbed7ef2946979d5
Craig Belcher <craig.belcher@belcherinc.com>
2026-06-16 06:34:28 -0400

    v16.0, 6/15/2026 Locking down Max Current to its own Grid.Column. It was bouncing left and right.

--- PicoPowerMonitor/MainWindow.xaml.cs
+++ PicoPowerMonitor/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-// v15.1, 6/15/2026 Minor UI changes to Max Current StackPanel.
+// v16.0, 6/15/2026 Locking down Max Current to its own Grid.Column. It was bouncing left and right.
 using Microsoft.UI.Composition;
 using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
